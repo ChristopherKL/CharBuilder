f66ce0940fef59a1ab5779ac9a11abf09052b893
Philipp Clausing <philipp.clausing@web.de>
2017-11-13 14:18:19 +0100

    Navigation Drawer & CardView

--- app/src/main/java/com/chrisphil/charbuilder/MainActivity.kt
+++ app/src/main/java/com/chrisphil/charbuilder/MainActivity.kt
@@ -31,22 +31,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
     private fun createList(size : Int) : List<CharInfo>{
         var liste : MutableList<CharInfo> = mutableListOf<CharInfo>()
         for (i in 1..size){
--- app/src/main/java/com/chrisphil/charbuilder/MainNavigationDrawer.kt
+++ app/src/main/java/com/chrisphil/charbuilder/MainNavigationDrawer.kt
@@ -1,12 +1,12 @@
 package com.chrisphil.charbuilder
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main_navigation_drawer.*
 import kotlinx.android.synthetic.main.app_bar_main_navigation_drawer.*
@@ -18,17 +18,21 @@ class MainNavigationDrawer : AppCompatActivity(), NavigationView.OnNavigationIte
         setContentView(R.layout.activity_main_navigation_drawer)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+        var recList = findViewById<RecyclerView>(R.id.cardList) as RecyclerView
+        recList.setHasFixedSize(true)
+        var llm = LinearLayoutManager(this)
+        llm.orientation = LinearLayoutManager.VERTICAL
+        recList.layoutManager = llm
+
+        var ca : CharAdapter = CharAdapter(createList(30))
+        recList.adapter = ca
     }
 
     override fun onBackPressed() {
@@ -39,46 +43,40 @@ class MainNavigationDrawer : AppCompatActivity(), NavigationView.OnNavigationIte
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main_navigation_drawer, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_chars -> {
                 // Handle the camera action
+                this.title = "Charaktere"
             }
-            R.id.nav_gallery -> {
-
+            R.id.nav_rulesets -> {
+                this.title = "Regelwerke"
             }
-            R.id.nav_slideshow -> {
-
+            R.id.nav_dice -> {
+                this.title = "Würfeltool"
             }
             R.id.nav_manage -> {
-
+                this.title = "Einstellungen"
             }
-            R.id.nav_share -> {
-
+            R.id.nav_import -> {
+                this.title = "Import"
             }
-            R.id.nav_send -> {
-
+            R.id.nav_export -> {
+                this.title = "Export"
             }
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun createList(size : Int) : List<CharInfo>{
+        var liste : MutableList<CharInfo> = mutableListOf<CharInfo>()
+        for (i in 1..size){
+            var ci : CharInfo = CharInfo("Peter Zwegert" + i,"Star Wars : Am Rande des Imperiums")
+            liste.add(ci)
+        }
+        return liste
     }
 }
