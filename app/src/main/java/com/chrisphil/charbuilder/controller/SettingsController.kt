0c5363dd0de298a1e637b83b78ce90639b625ca5
PhilippCl <philipp.clausing@fh-bielefeld.de>
2017-11-27 14:38:55 +0100

    Added Preferences Activity & Dialog fix & TabBar Fix

--- app/src/main/java/com/chrisphil/charbuilder/controller/SettingsController.kt
+++ app/src/main/java/com/chrisphil/charbuilder/controller/SettingsController.kt
@@ -1,7 +1,21 @@
 package com.chrisphil.charbuilder.controller
 
+import android.os.Bundle
+import android.support.v7.preference.PreferenceFragmentCompat
+import com.chrisphil.charbuilder.R
+
 /**
  * Created by Phil on 24.11.2017.
  */
-class SettingsController {
+class SettingsController : PreferenceFragmentCompat(){
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences,rootKey)
+    }
+
+    companion object {
+        fun newInstance(): SettingsController {
+            return SettingsController()
+        }
+    }
+
 }
