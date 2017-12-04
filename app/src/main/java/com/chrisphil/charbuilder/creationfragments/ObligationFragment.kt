7d8e297011d5ef42533c869ff0b0fe86b359504e
Christopher <christopher.kluck@web.de>
2017-12-04 14:50:47 +0100

    Interface OnDataPass, CharCreationController Refactoring

--- app/src/main/java/com/chrisphil/charbuilder/creationfragments/ObligationFragment.kt
+++ app/src/main/java/com/chrisphil/charbuilder/creationfragments/ObligationFragment.kt
@@ -1,9 +1,48 @@
 package com.chrisphil.charbuilder.creationfragments
 
+import android.content.Context
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.chrisphil.charbuilder.interfaces.OnDataPass
 
 /**
  * Created by Christopher on 04.12.2017.
  */
 class ObligationFragment : Fragment() {
+
+    lateinit var dataPass : OnDataPass
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        dataPass = context as OnDataPass
+    }
+
+    companion object {
+
+        private val ARG_SECTION_NUMBER = "section_number"
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        fun newInstance(sectionNumber: Int): ObligationFragment {
+            val fragment = ObligationFragment()
+            val args = Bundle()
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onDestroyView() {
+        dataPass.onDataPass("TODO")
+        super.onDestroyView()
+    }
 }
