8a9b0b05b07d670f9e9bd5965de73d0efadd045f
Christopher <christopher.kluck@web.de>
2017-12-04 13:50:04 +0100

    Created CharCreation Fragments

---
+++ app/src/main/java/com/chrisphil/charbuilder/creationfragments/SpeciesFragment.kt
@@ -0,0 +1,9 @@
+package com.chrisphil.charbuilder.creationfragments
+
+import android.support.v4.app.Fragment
+
+/**
+ * Created by Christopher on 04.12.2017.
+ */
+class SpeciesFragment : Fragment() {
+}
