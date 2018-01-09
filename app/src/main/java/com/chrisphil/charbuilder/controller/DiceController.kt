225f098bd9e0ea7b3c59a8f2e3eb62c06232ff74
Philipp Clausing <philipp.clausing@web.de>
2018-01-09 22:00:50 +0100

    Added Dice View & Quick Fix for Obligation Fragment

--- app/src/main/java/com/chrisphil/charbuilder/controller/DiceController.kt
+++ app/src/main/java/com/chrisphil/charbuilder/controller/DiceController.kt
@@ -1,10 +1,32 @@
 package com.chrisphil.charbuilder.controller
 
-import android.app.Fragment
+import android.support.v4.app.Fragment
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.chrisphil.charbuilder.R
 
 /**
  * Created by Phil on 24.11.2017.
  */
 class DiceController : Fragment() {
 
+    companion object {
+        fun newInstance(): DiceController {
+            return DiceController()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View = inflater!!.inflate(R.layout.fragment_dice,container,false)
+
+        return view
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+    }
 }
