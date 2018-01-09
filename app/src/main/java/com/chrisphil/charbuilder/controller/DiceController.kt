a02d1fd8bb5e72fc9c425e23187e66a1bff60516
Philipp Clausing <philipp.clausing@web.de>
2018-01-09 20:59:48 +0100

    Added Images for the different Dice

--- app/src/main/java/com/chrisphil/charbuilder/controller/DiceController.kt
+++ app/src/main/java/com/chrisphil/charbuilder/controller/DiceController.kt
@@ -1,7 +1,10 @@
 package com.chrisphil.charbuilder.controller
 
+import android.app.Fragment
+
 /**
  * Created by Phil on 24.11.2017.
  */
-class DiceController {
+class DiceController : Fragment() {
+
 }
